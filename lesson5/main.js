0db158145bb9a53bc6be4ca141261c4f4698aa4e
otaki444 <xm.ms.430x@gmail.com>
2020-06-09 23:31:30 +0900

    [lesson]変数

---
+++ lesson5/main.js
@@ -0,0 +1,24 @@
+let myName = '鈴木';
+alert(myName);
+// myNameに別の値を再代入
+myName = '高橋';
+alert(myName);
+
+let currentDate = new Date();
+alert('現在' + currentDate + 'です');
+
+let number1 = 10;
+let number2 = 20;
+let sum = number1 + number2;
+console.log(sum); //結果30
+
+let familyName = '鈴木';
+let firstName = '太郎';
+let fullName = familyName + firstName;
+console.log(fullName); //結果鈴木太郎
+
+
+// 変数を別に変数に代入
+let value1 = 100;
+let value2 = value1;
+console.log(value2);
