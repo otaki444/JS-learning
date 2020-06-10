211f07442d8258570b138d857860d7ec58df01ab
otaki444 <xm.ms.430x@gmail.com>
2020-06-10 23:04:02 +0900

    [lesson]プログラムに対してコメント

---
+++ lesson7/main.js
@@ -0,0 +1,17 @@
+const value = 100;
+const tax = 1.1;
+
+// 消費税込みの値段
+const price = value * tax;
+
+// プログラム内コメント
+const result = 100 + 200 /* + 300 */ + 400;
+console.log(result); // 結果700
+
+/* 複数行内の
+改行でも有効 */
+
+/*
+*体裁を整えて
+*使用することもできる
+*/
