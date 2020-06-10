cc0207e7f0f613b1a9f3b1953d7d8b881826cf8c
otaki444 <xm.ms.430x@gmail.com>
2020-06-10 22:35:39 +0900

    [lesson]定数

---
+++ lesson6/main.js
@@ -0,0 +1,12 @@
+const myName = '鈴木';
+alert(myName);
+
+const number1 = 10;
+const number2 = 20;
+const sum = number1 + number2;
+console.log(sum); //結果30 固定
+
+const familyName = '鈴木';
+const firstName = '太郎';
+const fullName = familyName + firstName;
+console.log(fullName); //結果鈴木太郎 固定
