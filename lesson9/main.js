b84640a742973507a02cb65a883d77ee4e310d5d
otaki444 <xm.ms.430x@gmail.com>
2020-06-11 00:10:35 +0900

    [lesson]複合代入演算子

---
+++ lesson9/main.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+a += b; //a = a + bと同じ
+console.log(a);
+
+let c = '鈴木';
+let d = '太郎';
+c += d;
+console.log(c); //結果 鈴木太郎
+
+let e = 2;
+let f = 5;
+e *= f;
+console.log(e); //結果 10
