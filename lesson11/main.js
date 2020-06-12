eb634f435db023bd9f272c473dc93bfe7040af2c
otaki444 <xm.ms.430x@gmail.com>
2020-06-12 22:47:22 +0900

    [lesson]アロー関数

---
+++ lesson11/main.js
@@ -0,0 +1,16 @@
+const calcSum = (a, b, c) => {
+    const result = a + b + c;
+    return result;
+};
+
+const myFunction1 = (a) => {
+    return a + 2;
+};
+
+// （）を省略
+const myFunction2 = a => {
+    return a + 2;
+};
+
+// １行の場合
+const myFunction3 = (a) => a + 2;
