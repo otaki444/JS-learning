ae1730dab720dba41cce93927b599c6ac26e016b
otaki444 <xm.ms.430x@gmail.com>
2020-06-15 00:25:41 +0900

    [lesson]四捨五入、切り捨て、切り上げ

---
+++ lesson19/main.js
@@ -0,0 +1,37 @@
+// Math.round(数値) 四捨五入
+const round1 = Math.round(6.3);
+console.log(round1); // 結果 6
+
+const round2 = Math.round(4.8);
+console.log(round2); //結果 5
+
+const round3 = Math.round(-7.49);
+console.log(round3); //結果 -7
+
+const round4 = Math.round(-7.50);
+console.log(round4); //結果 -7
+
+const round5 = Math.round(-7.51);
+console.log(round5); //結果 -8
+
+// Math.floor(数値) 切り捨て
+const floor1 = Math.floor(6.3);
+console.log(floor1); // 結果 6
+
+const floor2 = Math.floor(4.8);
+console.log(floor2); //結果 4
+
+// Math.ceil(数値) 切り上げ
+const ceil1 = Math.ceil(6.3);
+console.log(ceil1); // 結果 7
+
+const ceil2 = Math.ceil(4.8);
+console.log(ceil2); //結果 5
+
+// Math.trunc(数値) 整数部分を返す
+const trunc1 = Math.trunc(6.3);
+console.log(trunc1); // 結果 6
+
+const trunc2 = Math.trunc(4.8);
+console.log(trunc2); //結果 4
+
