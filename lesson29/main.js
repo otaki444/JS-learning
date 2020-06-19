a659882341551b15ae80ce719ad975d8328ee49d
otaki444 <xm.ms.430x@gmail.com>
2020-06-19 18:30:00 +0900

    [lesson]指定範囲の文字列を取り出す

---
+++ lesson29/main.js
@@ -0,0 +1,23 @@
+// slice(開始インデックス, 終了インデックス) 指定範囲内の文字列を取得
+// substring(開始インデックス, 終了インデックス) 指定範囲内の文字列を取得
+
+'JavaScript'.slice(0, 4); //Java
+'JavaScript'.slice(0); //JavaScript
+
+'JavaScript'.substring(0, 4); //Java
+'JavaScript'.substring(0); //JavaScript
+
+// sliceの場合
+'鈴木のポートフォリオ'.slice(3, -4); //ポート
+'鈴木のポートフォリオ'.slice(-4, -1); //フォリ
+// substringの場合(マイナス値が0になる  )
+'鈴木のポートフォリオ'.substring(3, -4); //鈴木の
+'鈴木のポートフォリオ'.substring(-4, -1); //範囲外のため ''空文字が返る
+
+/**
+ * slice substringの違い
+ * 開始インデックスが終了インデックスより大きくなった際に挙動が変化
+ * substringの場合、開始インデックスと終了インデックスを入れ替えて処理される
+*/
+'ようこそ日本へ'.slice(3, 1); //結果''空文字が返る
+'ようこそ日本へ'.substring(3, 1); //1と3が入れ替わり結果、'うこ'が入る
