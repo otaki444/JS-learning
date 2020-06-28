8098fd099de4025fe7eaaa4410ad58f7292814b0
otaki444 <xm.ms.430x@gmail.com>
2020-06-28 20:46:48 +0900

    [lesson]配列の長さを取得

---
+++ lesson43/main.js
@@ -0,0 +1,8 @@
+// 配列.length 配列の長さを取得する
+
+const array1 = ['鈴木', '佐藤', '高橋'];
+console.log(array1.length); //結果: 3
+
+const array2 = [{id: 1, name: 'りんご'}, {id: 2, name: 'オレンジ'}];
+console.log(array2.length);// 結果: 2
+
