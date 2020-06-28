ad58d799457a30fc8da6e9d2f32594d572d50e31
otaki444 <xm.ms.430x@gmail.com>
2020-06-29 00:00:23 +0900

    [lesson]配列の各要素に対して処理(インデックス不要)

---
+++ lesson45/main.js
@@ -0,0 +1,8 @@
+// for (const 要素 of 配列) for...ofでループ
+
+const array = ['いちご', 'みかん', 'りんご'];
+
+// 配列の各要素についてループ
+for (const value of array) {
+    console.log(value); //結果: 'いちご', 'みかん', 'りんご'が順に出力
+}
