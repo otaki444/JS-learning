ffce8730474968d4232cc20eb1733c4f6d6d55d4
otaki444 <xm.ms.430x@gmail.com>
2020-06-29 17:33:42 +0900

    [lesson]配列を各要素に対して処理(インデックス不要)2

---
+++ lesson46/main.js
@@ -0,0 +1,10 @@
+const array = ['イチゴ', 'みかん', 'リンゴ'];
+
+// 配列の長さを取得する
+const arrayLength = array.length;
+
+// 配列の各種について処理
+for (let i = 0; i < arrayLength; i++) {
+    // インデックスのiの要素を出力
+    console.log(array[i]); // 'イチゴ', 'みかん', 'リンゴ'が順に出力
+}
