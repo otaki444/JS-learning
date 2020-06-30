367bce9962dfb42d1604c37301a4c672e196095d
otaki444 <xm.ms.430x@gmail.com>
2020-07-01 04:45:35 +0900

    [lesson]配列の要素を結合し文字列として出力

---
+++ lesson51/main.js
@@ -0,0 +1,7 @@
+// 配列.join(結合文字列) 配列の各要素を結合して文字列にする
+
+const array1 = [2, 4 ,10];
+console.log(array1.join()); // 結果: 2,4,10(文字列)
+
+const array2 = ['a', 'b', 'c'];
+console.log(array2.join('')); // 結果: abc
