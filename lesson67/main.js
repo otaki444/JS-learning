bc685be0f27bd634d508cd36f50bbc7d79a6ee48
otaki444 <xm.ms.430x@gmail.com>
2020-07-08 01:45:58 +0900

    [lesson]配列の各要素に対しての繰り返し処理

---
+++ lesson67/main.js
@@ -0,0 +1,13 @@
+const numbers = [25, 50, 75, 100];
+
+for (let i = 0; i < numbers.length; i++) {
+    // 配列に対して順に繰り返し処理が行われる
+    console.log(`${i + 1}番: ${numbers[i]}`);
+}
+
+const persons = ['田中', '鈴木', '佐藤', '高橋'];
+
+for (let i = 0; i < persons.length; i++) {
+    // 配列に対して順に繰り返し処理が行われる
+    console.log(`${i + 1}番: ${persons[i]}さん`);
+}
