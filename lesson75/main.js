4781336d6d1afbdd4cce274210ab429db0075745
otaki444 <xm.ms.430x@gmail.com>
2020-07-13 22:12:12 +0900

    [lessson]データ型の確認

---
+++ lesson75/main.js
@@ -0,0 +1,26 @@
+// typeof 値 データ型を確認する
+
+/** データ型
+ * Undefined   undefined
+ * Null        null
+ * Boolean     true, false
+ * String      'Hello World'
+ * Number      1, 2, 3
+ * Symbol      symbol() (唯一のもの) 「===」や「==」でtrueになるものを作らない
+ * Object      [1, 2, 3], {id: 1, name: 'john doe'}
+ * 関数         function() {}
+ */
+
+console.log(typeof undefined);// 結果: undefined
+
+console.log(typeof true);// 結果: boolean
+
+console.log(typeof 'Hello World');// 結果: string
+
+console.log(typeof 1);// 結果: number
+
+console.log(typeof Symbol());// 結果: symbol
+
+console.log(typeof [1, 2, 3, 4, 5]);// 結果: object
+
+console.log(typeof function() {});// 結果: function
