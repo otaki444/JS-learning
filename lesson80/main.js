c35c12a1339409951f76df9991c1d4d320983237
otaki444 <xm.ms.430x@gmail.com>
2020-07-17 00:02:58 +0900

    [lesson]undefinedについて

---
+++ lesson80/main.js
@@ -0,0 +1,12 @@
+// undefined 値が未定であること
+
+let box;
+console.log(box); // 結果: undefined
+
+const object = {};
+console.log(object.content);
+
+function myFunction(f) {
+    console.log(`${f}です`);
+}
+myFunction();
