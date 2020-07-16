0d2fb050db653d6cd289ab85313c38a1c304ab6e
otaki444 <xm.ms.430x@gmail.com>
2020-07-16 23:48:27 +0900

    [lesson]型を変換する

---
+++ lesson79/main.js
@@ -0,0 +1,14 @@
+// Boolean() 値を真偽値型に変換する
+// String() 値を文字列型に変換する
+// Number() 値を数値型に変換する
+// parseInt() 文字列を数値型(整数)に変換する
+// parseFloat() 文字列を数値型(浮動小数点)に変換する
+
+console.log(Boolean()); //結果: false
+console.log(Boolean(1)); //結果: true
+
+console.log(String(100)); //結果: "100"
+console.log(Number('100')); //結果: 100
+
+console.log(parseInt('123.456')); //結果: 123
+console.log(parseFloat('123.456')); //結果: 123.456
