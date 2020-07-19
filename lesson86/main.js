8a27229b3f9feed990628b780ce24b5aaaa97aef
otaki444 <xm.ms.430x@gmail.com>
2020-07-20 00:29:18 +0900

    [lesson]タイムスタンプ値を取得

---
+++ lesson86/main.js
@@ -0,0 +1,7 @@
+// Date.parse() 日付文字列からタイムスタンプ値を取得する
+
+const today = Date.parse('2020/07/20');
+console.log(today); // 結果: 1595170800000
+
+const now = Date.now();
+console.log(now); // 結果: 1595089446025
